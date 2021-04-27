8694ae278d35d18cb4c58abc763dc612ce9a05b7
shimakee <shimakee@gmail.com>
2021-04-27 18:10:44 +0800

    Created Jump script
    Created input action map
    Created InputHanlder script
    
    Added inputHanlder as component to bird.
    Added Player input component to bird
    
    Connected Input Action with script input handler.

---
+++ Assets/scripts/InputHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+            Jump();
+    }
+
+    private void Jump()
+    {
+        Debug.Log("jump");
+    }
+}
