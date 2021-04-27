92c5d51318996d0df1d7883800df749066d796a4
shimakee <shimakee@gmail.com>
2021-04-27 18:34:51 +0800

    Added pipe spawner
    Pipe now spawns.

---
+++ Assets/scripts/PipeSpawner.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class PipeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pipePrefab;
+    [SerializeField]
+    private Vector2 spawnPosition;
+    [SerializeField]
+    private float spawnTimer = 5;
+    private float time;
+
+    private void Awake()
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pipePrefab == null)
+            throw new NullReferenceException("Game object pipe prefab should not be null");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        if(time > spawnTimer)
+        {
+            time = 0;
+            var pipe = GameObject.Instantiate(pipePrefab);
+            pipe.transform.position = spawnPosition;
+        }
+
+    }
+}
